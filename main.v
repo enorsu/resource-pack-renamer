820f150f9c882040521fad9e43c36042ebbe08ec
enorsu <pelaaja538@gmail.com>
2025-11-27 19:22:42 +0200

    removed uselessness

--- main.v
+++ main.v
@@ -6,23 +6,27 @@ import flag
 
 
 fn main() {
-
+    // define blacklisted letters
     blacklist := ["[", "]", "(", ")", "!", "§", "¡", "&"]
 
+    // initialize flag(argument) parser
     mut fp := flag.new_flag_parser(os.args)
 
+    
+    // set version
     fp.application("resourcepackrenamer")
     fp.version("0.0.1")
     fp.description("rename my packs for me")
 
+    // argument(s)
     path := fp.string("path", `p`, "none", "path for processing")
 
-
+    // if user didn't provide a path; return
     if path == "none" {
         println("a file path is required.")
         return
     }
-
+    // loop through all the files in the path
     for mut file in os.ls(path) or { [] } {
         oldfile := file
         // remove blacklisted stuffs
